bdcada38efd5f44cd61466c5e5ec8232b43c22d9
MacleChen <851327579@qq.com>
2019-10-10 11:40:02 +0800

    Cookie本地持久化

---
+++ MyWKWebViewDemoTests/MyWKWebViewDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  MyWKWebViewDemoTests.m
+//  MyWKWebViewDemoTests
+//
+//  Created by 陈帆 on 2019/10/9.
+//  Copyright © 2019 陈帆. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyWKWebViewDemoTests : XCTestCase
+
+@end
+
+@implementation MyWKWebViewDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
